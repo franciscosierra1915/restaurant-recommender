9829eca3a2103f5936ad2de1fd48a94a6a4ce913
franciscosierra1915 <fransierra.s.15@gmail.com>
2022-04-11 18:58:27 -0500

    added restaurants

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
-import './style.css'
+import restaurants from './restaurants';
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const dollarSigns = '$$';
+const deliveryTimeMax = '90';
+const maxDistance = 10;
+let result;
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const priceBracket: number = dollarSigns.length;
+
+const filteredRestaurants = restaurants.filter((restaurant) => {
+  if (restaurant.priceBracket > priceBracket) {
+    return false;
+  }
+
+  if (restaurant.deliveryTimeMinutes > deliveryTimeMax) {
+    return false;
+  }
+
+  if (restaurant.distance > maxDistance) {
+    return false;
+  }
+
+  return restaurant;
+});
+
+if (filteredRestaurants.length === 0) {
+  result = 'There are no restaurants available right now.';
+} else {
+  result = `We found ${filteredRestaurants.length} restaurants, the first is ${filteredRestaurants[0].restaurantName}.`;
+}
+
+console.log(result);
